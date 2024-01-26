cf235fd1c6e65f033a40513952d01f41eb6e90c7
Matan Konstantini <matanko35@gmail.com>
2024-01-26 23:06:37 +0200

    updated readme and license

--- README.md
+++ README.md
@@ -1,46 +1,42 @@
 # InstantOcean DEMO Version
-A DEMO of my cruise line company web project.  
-The DEMO does not support any backend functionality of the original website.
-Cruise and user data exists as a client-side object.
+A DEMO of my cruise line company web project.
+See full project at: [https://github.com/MKonstantini/InstantOcean](https://github.com/MKonstantini/InstantOcean)   
+The DEMO does not support any backend functionality available in the full project.   
 
 Browse and search cruise lines and pricings.  
-Register or use demo users for favorites and admin tool features.  
-Includes checkout page for reviewing and finalizing payment options.  
+Use demo users to view the favorites and admin tool pages.  
+Includes checkout page for reviewing selected cruise.  
 
-Account types:  
-* Regular: Add to favorites and explore the favorites page.
-* Admin: Includes feature to manage cruises' data.
+Account types (Full Project):  
+* Regular: Add to favorites and explore the favorites page.  
+* Admin: Includes feature to manage cruises' data.  
 
 ## Technologies
-* REACT TypeScript
-* Express
-* MongoDB
-* CSS
-* Bootstrap v5.3
-* Font Awesome
+* REACT TypeScript  
+* Express  
+* MongoDB  
+* CSS  
+* Bootstrap v5.3  
+* Font Awesome  
 
-## Installation
-1. Clone the repository:
-   ``` git clone mkonstantini.github.io/InstantOcean/ ```
-2. Navigate to the project directory using the ``` cd ``` terminal command.
-3. Install dependencies:
-   ``` npm install ```
-4. IMPORTANT! Run server locally:
-   ``` nodemon server ```
-5. Run client:
-   ``` npm start ```
+## Installation  
+1. Clone the repository:  
+   ``` git clone mkonstantini.github.io/Companix/ ```  
+2. Navigate to the project directory using the ``` cd ``` terminal command.  
+3. Install dependencies:  
+   ``` npm install ```  
 
 ## Backend:
 Backend developed in Express.  
-User and cruises data stored in a MongoDB database.
+User and cruises data stored in a MongoDB database.  
 Project's .env files (client + server) are included in the project files.  
-Run backend locally using: ```nodemon server```
+Run backend locally using: ```nodemon server```  
 
-## Available Scripts
+## Available Scripts  
 ```npm start```   
 ```npm test```   
 ```npm run build```    
 ```npm run eject```   
 
-## License
+## License  
 This project is licensed under the [MIT Licence](https://choosealicense.com/licenses/mit/).
--- src/DataBackup.ts
+++ src/DataBackup.ts
@@ -3,36 +3,68 @@ import User from "./interfaces/User"
 
 let cruises_data : Cruise[] = [
     {
-        name: "Bahamas Blue",
-        duration: 4,
-        departFrom: "Miami, FL",
-        ports: "Half Moon Cay, Nassau",
-        img: "https://images.pexels.com/photos/813011/pexels-photo-813011.jpeg?auto=…",
-        startDate: '2023-12-27',
-        startPrice: 199,
-        cruiseNum: 1
+      name: "Bahamas Blue",
+      duration: 4,
+      departFrom: "Miami, FL",
+      ports: "Half Moon Cay, Nassau",
+      img: "https://images.pexels.com/photos/813011/pexels-photo-813011.jpeg?auto=…",
+      startDate: '2023-12-27',
+      startPrice: 199,
+      cruiseNum: 1
     },
     {
-        name: "Mexico Crucero",
-        duration: 5,
-        departFrom: "Long Beach, LA",
-        ports: "Catalina Island, Ensenada",
-        img: "https://images.pexels.com/photos/775294/pexels-photo-775294.jpeg?auto=…",
-        startDate: '2024-01-10',
-        startPrice: 249,
-        cruiseNum: 2
+      name: "Mexico Crucero",
+      duration: 5,
+      departFrom: "Long Beach, LA",
+      ports: "Catalina Island, Ensenada",
+      img: "https://images.pexels.com/photos/775294/pexels-photo-775294.jpeg?auto=…",
+      startDate: '2024-01-10',
+      startPrice: 249,
+      cruiseNum: 2
+    },
+    {
+      name: "Panama Magic",
+      duration: 5,
+      departFrom: "Tampa, FL",
+      ports: "Limon, Grand Cayman",
+      img: "https://images.pexels.com/photos/1814472/pexels-photo-1814472.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      startDate: '2024-02-05',
+      startPrice: 399,
+      cruiseNum: 3
+    },
+    {
+      name: "Hawaii Vacation",
+      duration: 7,
+      departFrom: "Long Beach, LA",
+      ports: "Maui, Oahu, Kauai",
+      img: "https://images.pexels.com/photos/358229/pexels-photo-358229.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      startDate: '2024-01-21',
+      startPrice: 429,
+      cruiseNum: 4
+    },
+    {
+      name: "Alaska Breeze",
+      duration: 5,
+      departFrom: "Seattle, WA",
+      ports: "Skagway, Juneau, Ketchikan",
+      img: "https://images.unsplash.com/photo-1511316695145-4992006ffddb?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      startDate: '2024-02-24',
+      startPrice: 699,
+      cruiseNum: 5
+    },
+    {
+      name: "Icefjord Explorer",
+      duration: 7,
+      departFrom: "Baltimore, MD",
+      ports: "Sisimiut, Ilulissat, Nuuk",
+      img: "https://media.npr.org/assets/img/2023/09/14/ap23257266606444_wide-c86d9b7e17b5a2fc29727784b270981a462c4ae6-s800-c85.webp",
+      startDate: '2024-02-11',
+      startPrice: 719,
+      cruiseNum: 6
     }
 ]
 
 let users_data : User[] = [
-    {
-        firstname: "Matan",
-        lastname: "Konstantini",
-        email: "matanko35@gmail.com",
-        password: "password1",
-        accountType: "admin",
-        favorites: [1]
-    },
     {
         firstname: "demoRegular",
         lastname: "demoRegular",
